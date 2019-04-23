4268de772b8dc333ed35cca9205bf98f69b6fe35
John Hayes <johnnyhayes7@gmail.com>
2019-04-23 18:11:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is John Hayes"
+  end
+  
+  get '/hometown' do
+    "My hometown is Yorktown H"
 end
